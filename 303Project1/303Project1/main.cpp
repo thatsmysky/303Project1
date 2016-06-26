8e66b4e08bbfbf4983581ce58460ccb4cd5c5dd9
John Buckley V <jbuckleyv@gmail.com>
2016-06-26 18:23:05 -0500

    first commit

---
+++ 303Project1/303Project1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  303Project1
+//
+//  Created by John Buckley on 6/23/16.
+//  Copyright © 2016 John Buckley V. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
