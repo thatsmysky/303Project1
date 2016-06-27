ddf578e4621a753609f8515990bcab5da2fea304
Lucy Kull <lucy.r.kull@gmail.com>
2016-06-27 09:57:31 -0500

    Update polynomial.cpp

--- 303Project1/303Project1/polynomial.cpp
+++ 303Project1/303Project1/polynomial.cpp
@@ -6,22 +6,44 @@
 #include <iostream>
 using namespace std;
 
-void polynomial::sort()
+
+list<Term> polynomial::operator+ (list<Term> &other)
 {
-    
+    list<Term> result;
+    list<Term>::iterator polyIndex = poly.begin();
+    list<Term>::iterator otherIndex = other.begin();
+    while (polyIndex != poly.end() && otherIndex != other.end())
+    {
+        if (polyIndex->getExp() > otherIndex->getExp())
+        {
+            result.push_back(*polyIndex);
+            polyIndex++;
+        }
+        
+        else if (polyIndex->getExp() == otherIndex->getExp())
+        {
+            int buff = polyIndex->getCoe() + otherIndex->getCoe();
+            int ffer = polyIndex->getExp();
+            Term dummy = Term(buff, ffer);
+            result.push_back(dummy);
+        }
+        
+        else
+        {
+            result.push_back(*otherIndex);
+            otherIndex++;
+        }
+    }
+    return result;
 }
 
-polynomial operator+ (const polynomial& other)
-{
-    other.sort();
-}
-
-ostream &operator<<(ostream &out, polynomial& poly)
+ostream &operator<<(ostream &out, list<Term> &poly)
 {
     for (list<Term>::iterator i = poly.begin(); i != poly.end(); i++)
     {
         out << *i;
     }
+    return out;
 }
 
 
