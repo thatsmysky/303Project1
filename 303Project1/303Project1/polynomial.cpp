0c07261f2cc434e790b093bb183db3f7cad00624
Lucy Kull <lucy.r.kull@gmail.com>
2016-06-28 15:21:22 -0500

    Update polynomial.cpp

--- 303Project1/303Project1/polynomial.cpp
+++ 303Project1/303Project1/polynomial.cpp
@@ -2,52 +2,124 @@
 //  polynomial.cpp
 //
 
-#include "polynomial.h"
+#include "polynomial.hpp"
 #include <iostream>
 using namespace std;
 
 
-list<Term> polynomial::operator+ (list<Term> &other)
+polynomial::polynomial()
 {
-    list<Term> result;
-    list<Term>::iterator polyIndex = poly.begin();
-    list<Term>::iterator otherIndex = other.begin();
-    while (polyIndex != poly.end() && otherIndex != other.end())
+    head->coe = 0;
+    head->exp = 0;
+    head->next = NULL;
+}
+
+polynomial::polynomial(Term t)
+{
+    head->setCoe(t.getCoe());
+    head->exp = t.getExp();
+    head->next = NULL;
+}
+
+
+
+polynomial polynomial::operator+ (polynomial &other)
+{
+    polynomial result;
+    Term *polyIndex = head;
+    Term *otherIndex = other.head;
+    while (polyIndex != NULL && otherIndex != NULL)
     {
-        if (polyIndex->getExp() > otherIndex->getExp())
+        if (result.head->getExp() == 0 && result.head->getExp() == 0)
         {
-            result.push_back(*polyIndex);
-            polyIndex++;
+            if (polyIndex->getExp() > otherIndex->getExp())
+            {
+                result.head->setCoe(polyIndex->getCoe());
+                result.head->setExp(polyIndex->getExp());
+                polyIndex = polyIndex->next;
+            }
+            
+            else if (polyIndex->getExp() == otherIndex->getExp())
+            {
+                int buff = polyIndex->getCoe() + otherIndex->getCoe();
+                int ffer = polyIndex->getExp();
+                result.head->setCoe(buff);
+                result.head->setExp(ffer);
+                polyIndex = polyIndex->next;
+                otherIndex = otherIndex->next;
+            }
+            
+            else
+            {
+                result.head->setCoe(otherIndex->getCoe());
+                result.head->setExp(otherIndex->getExp());
+                otherIndex = otherIndex->next;
+            }
         }
         
-        else if (polyIndex->getExp() == otherIndex->getExp())
+        if ((polyIndex != NULL && otherIndex != NULL) &&(polyIndex->getExp() > otherIndex->getExp()))
+        {
+            result.push_back(*polyIndex);
+            polyIndex = polyIndex->next;
+        }
+        
+        else if ((polyIndex != NULL && otherIndex != NULL) && (polyIndex->getExp() == otherIndex->getExp()))
         {
             int buff = polyIndex->getCoe() + otherIndex->getCoe();
             int ffer = polyIndex->getExp();
             Term dummy = Term(buff, ffer);
             result.push_back(dummy);
+            polyIndex = polyIndex->next;
+            otherIndex = otherIndex->next;
         }
         
-        else
+        else if (polyIndex != NULL && otherIndex != NULL)
         {
             result.push_back(*otherIndex);
-            otherIndex++;
+            otherIndex = otherIndex->next;
         }
+    }
+    
+    while (polyIndex == NULL && otherIndex != NULL)
+    {
+        result.push_back(*otherIndex);
+        otherIndex = otherIndex -> next;
+    }
+    
+    while (polyIndex != NULL && otherIndex == NULL)
+    {
+        result.push_back(*polyIndex);
+        polyIndex = polyIndex -> next;
     }
     return result;
 }
 
-ostream &operator<<(ostream &out, list<Term> &poly)
+ostream &operator<<(ostream &out, polynomial &poly)
 {
-    for (list<Term>::iterator i = poly.begin(); i != poly.end(); i++)
+    for (Term *i = poly.head; i != NULL; i = i->next)
     {
         out << *i;
+        if (i->next->next != NULL)
+            out<< " + ";
     }
     return out;
 }
 
-
-polynomial::~polynomial()
+void polynomial::push_back(Term &t)
 {
-    poly.~list();
+    Term *newTerm = new Term;
+    newTerm->coe = t.coe;
+    newTerm->exp = t.exp;
+    newTerm->next = NULL;
+    
+    Term *curr = head;
+    while(curr)
+    {
+        if (curr->next == NULL)
+        {
+            curr->next = newTerm;
+            return;
+        }
+        curr = curr->next;
+    }
 }
