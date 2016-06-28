435e3dacfa57422a8024c88ff374985c2fb44ac3
John Buckley <jbuckleyv@gmail.com>
2016-06-27 22:49:15 -0500

    Add files via upload

--- 303Project1/303Project1/main.cpp
+++ 303Project1/303Project1/main.cpp
@@ -1,12 +1,14 @@
 // 303 Project 1
 // user menu and main
 
+#include "polynomial.h"
+#include "Term.h"
 #include <iostream>
 #include <list>
 using namespace std;
 
 // return whether the menu was processed correctly
-bool processMenu(string polynomial1, string polynomial2, int sum) {
+bool processMenu(polynomial polynomial1, polynomial polynomial2, polynomial finalPoly) {
     
     // show the main user the menu
     cout << "Polynomial Adder" << endl;
@@ -16,75 +18,96 @@ bool processMenu(string polynomial1, string polynomial2, int sum) {
     cout << "Please Enter a Number: ";
     
     // response variables
-    char choice1, choice2;
+    char choice1, exit1, exit2;
+    int polycoef, polyexp;
+    Term temp;
+    bool loopy = false;
     cin >> choice1;
     
     // enter polynomials, displays user entered polynomials for review
     // if user input correct return to main menu, if incorrect allow user to reenter polynomials
     if (choice1=='1') {
         cout << "Enter first polynomial: ";
-        cin >> polynomial1;
-        cout << "Enter second polynomial: ";
-        cin >> polynomial2;
-        cout << "You entered: " + polynomial1 + " and " + polynomial2 << endl;
-        cout << " Is this correct?" << endl;
-        cout << "(1) Yes" << endl;
-        cout << "(2) No" << endl;
-        cout << "Please Enter a number: ";
-        cin >> choice2;
-        
-        while (choice2 != '1'){
-            if(choice2=='2'){
-                cout << "Enter first polynomial: ";
-                cin >> polynomial1;
-                cout << "Enter second polynomial: ";
-                cin >> polynomial2;
-                cout << "You entered: " + polynomial1 + " and " + polynomial2;
-                cout << " Is this correct?" << endl;
-                cout << "(1) Yes" << endl;
-                cout << "(2) No" << endl;
-                cout << "Please Enter a number: ";
-                cin >> choice2;
-    } else {
-        cout << "Your input was not valid, returning to main menu." << endl;
-        choice2 = '1';
+        while (loopy != true){ // bool for menu options
+            cout << "Enter a coefficent: " << endl;
+            cin >> polycoef;
+            cout << "Enter an exponent: " << endl;
+            cin >> polyexp;
+            
+            Term(polycoef, polyexp);
+            polynomial1 = polynomial1 + temp;
+            
+            cout << "Enter another term in the polynomial? ";
+            cout << "(Y)es" << endl;
+            cout << "(N)o" << endl;
+            cout << "Please enter Y or N: ";
+            
+            cin >> exit1;
+            
+            if (exit1 == 'y' || exit1 == 'Y'){
+                loopy = false;
+            } else if (exit1 == 'n' || exit1 == 'N'){
+                loopy = true;
+            } else {
+                loopy = true;
+            } }
+            
+            
+            loopy = true;
+            cout << "Enter second polynomial: ";
+            while (loopy != true){ // bool for menu options
+                cout << "Enter a coefficent: " << endl;
+                cin >> polycoef;
+                cout << "Enter an exponent: " << endl;
+                cin >> polyexp;
+                
+                Term(polycoef, polyexp);
+                polynomial2 = polynomial2 + temp;
+                
+                cout << "Enter another term in the polynomial? ";
+                cout << "(Y)es" << endl;
+                cout << "(N)o" << endl;
+                cout << "Please enter Y or N: ";
+                
+                cin >> exit2;
+                
+                if (exit2 == 'y' || exit2 == 'Y'){
+                    loopy = false;
+                } else if (exit2 == 'n' || exit2 == 'N'){
+                    loopy = true;
+                } else {
+                    loopy = true;
+                } }
+                return false;
+                
+                // adds both polynomials and displays result
+            } else if (choice1=='2') {
+                cout << "Adding polynomials" << endl;
+                finalPoly = polynomial1 + polynomial2;
+                cout << "The result is: " + finalPoly << endl;
+                
+                // exit command
+            } else if (choice1=='3') {
+                cout << "Now Exiting Program." << endl;
+                
+                // if any other user input is entered, give error message and redisplay main menu options
+            } else {
+                cout << "Your input was not valid, please try again." << endl;
+                return false;
+            }
+            
+            return true;
+            
         }
-    }
         
-    if(choice2=='1'){
-        cout << "Your input was saved, returning to main menu." << endl;
-        return false;
-    }
-return false;
-        
-    // adds both polynomials and displays result
-    } else if (choice1=='2') {
-        cout << "Adding variables" << endl;
-        sum = 1;
-   
-    // exit command
-    } else if (choice1=='3') {
-        cout << "Now Exiting Program." << endl;
-    
-    // if any other user input is entered, give error message and redisplay main menu options
-    } else {
-        cout << "Your input was not valid, please try again." << endl;
-        return false;
-    }
-    
-return true;
-    
-}
-
-int main () {
-    
-    // strings to hold polynomials to be added and sum
-    string polynomial1, polynomial2;
-    int sum;
-    
-    // loop until the menu is processed or user is finished
-    while (!processMenu(polynomial1, polynomial2, sum)) {
-    }
-    
-    return 0;
-}
+        int main () {
+            
+            // strings to hold polynomials to be added and sum
+            polynomial polynomial1, polynomial2, finalPoly;
+            
+            // loop until the menu is processed or user is finished
+            while (!processMenu(polynomial1, polynomial2, finalPoly)) {
+            }
+            
+            return 0;
+        }
