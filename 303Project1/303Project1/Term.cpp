8617e9dc0843f8bcfb740313230ee4a494a42af6
jlculbreath <jeremy.culbreath@gmail.com>
2016-06-28 17:47:16 -0500

    Add files via upload

--- 303Project1/303Project1/Term.cpp
+++ 303Project1/303Project1/Term.cpp
@@ -13,50 +13,55 @@ using namespace std;
 //   One Term within a Polynomial
 
 bool operator<(const Term& lhs, const Term& rhs) {		//compares lhs < rhs exponents
-    if (lhs.exp < rhs.exp) return true;
-    return false;
+	if (lhs.exp < rhs.exp) return true;
+	return false;
 }
 
 bool operator>(const Term& lhs, const Term& rhs) {		//compares lhs > rhs exponents
-    if (lhs.exp > rhs.exp) return true;
-    return false;
+	if (lhs.exp > rhs.exp) return true;
+	return false;
 }
 
 bool operator==(const Term& lhs, const Term& rhs) {		//compares lhs == rhs exponents
-    if (lhs.exp == rhs.exp) return true;
-    return false;
+	if (lhs.exp == rhs.exp) return true;
+	return false;
 }
 
 ostream& operator<<(ostream& ostr, Term& term) {		//for outputting Term to cout
-    if (term.coe == 0) return ostr << "";		//do not display terms with 0 coefficients
-    
-    return ostr << term.coe << "x^" << term.exp << " ";		//outputs "_x^_ "
-    //remember to remove - sign and change + to - for negative numbers on polynomial output
+	if (term.coe == 0) return ostr << "";		//do not display terms with 0 coefficients
+	if (abs(term.coe) != 1); ostr << abs(term.coe);	//polynomial class ostream should look at - and makes + into -
+	if (term.exp != 0) ostr << "x";				//don not display x for 0 exponents
+	if (term.exp != 0 && term.exp != 1) ostr << "^" << term.exp;		//do not display ^ and exponent for 0 or 1 exponets
+	ostr << " ";		//space before next term
+	return ostr;		//outputs "_x^_ "
 }
 
 
 Term::Term() {		//default constructor
-    coe = 0;
-    exp = 0;
+	coe = 0;
+	exp = 0;
 }
 
 Term::Term(int arg1, int arg2) {		//regular constructor
-    coe = arg1;
-    exp = arg2;
+	coe = arg1;
+	exp = arg2;
 }
 
 int Term::getCoe() {
-    return coe;
+	return coe;
 }
 
 int Term::getExp(){
-    return exp;
+	return exp;
 }
 
 void Term::setCoe(int arg) {
-    coe = arg;
+	coe = arg;
 }
 
 void Term::setExp(int arg) {
-    exp = arg;
+	exp = arg;
 }
+
+
+
